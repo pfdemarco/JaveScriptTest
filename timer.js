6ee08729d9738f75c7f100995a84c25e70817183
Patrick DeMarco <pfdemarco@hotmail.com>
2021-01-08 22:40:02 -0500

    Adding html code for controls

--- timer.js
+++ timer.js
@@ -4,15 +4,24 @@
 //functions 
 //event listeners
 //callers of functions 
-var timeLeft = 10;//timer total num seconds
+var timeLeft = 100;//timer total num seconds
 var t = document.getElementById("timer");//get a ref to the div id timer on html form
 var res = document.getElementById("reset");//get a ref to the reset button on html form
-var sAgain = document.getElementById('startAGAIN');//get a ref to the start button on teh htm,l form
+var sAgain = document.getElementById('startAGAIN');//get a ref to the start button on html form
 var tAway = document.getElementById("takeAway");//get a ref to the take away button
 var hand;//set a variable to teh handle of the setInterval function
 
+//need handles to the 3 radio buttons and the click from the html page
+var ques1 = document.getElementById("q1");
+var ques2 = document.getElementById("q2");
+var ques3 = document.getElementById("q3");
+var subButton = document.getElementById("butSubmit");
+var sCORE;//the score!
+
+
+
 function start(){//make a function that starts the count down
-    timeLeft = 10;//reset the timer 
+    timeLeft = 100;//reset the timer 
     hand = setInterval(function() {//set the ref to the handle of setInterval
       //this is the function we are passing into setInterval  
       t.innerHTML = timeLeft;//set the div id timer on html form to the timer value
@@ -23,11 +32,20 @@ function start(){//make a function that starts the count down
       },1000  )//is the number of milliseconds to wait
 }
 
+//make the controls from the array 
+function MakeTestControls(){
+//set the question up 
+//set up the 3 controls with right and worng answers
+//setup login for if they choose worng then -10 from timer else +1 on correct vals
+
+
+}
+
 //when the user clicks on the reset button on the html form
 //clear the handle to the setInterval and set the value of the div id back top 10
 res.addEventListener("click", function(){
   clearInterval(hand);
-  t.innerHTML=10;
+  t.innerHTML=100;
 })
 
 //when the user clicks on the start button on the html form run the fucntion to start the timer again 
